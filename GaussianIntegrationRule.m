289e51d4b5bf6cbbc912443d96f6eb98fe291047
wgreene <w.h.greene@gmail.com>
2019-09-21 07:57:12 -0400

    Add license.

--- GaussianIntegrationRule.m
+++ GaussianIntegrationRule.m
@@ -1,6 +1,22 @@
+%  
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU General Public License
+%   as published by the Free Software Foundation; either version 3
+%   of the License, or (at your option) any later version.
+%  
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%   General Public License for more details.
+%  
+%   You should have received a copy of the GNU General Public License
+%   along with this library; if not, visit
+%   http://www.gnu.org/licenses/gpl.html or write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+% 
+% Copyright (C) 2016-2019 William H. Greene
+
 classdef GaussianIntegrationRule
-  %GAUSSIANINTEGRATIONRULE Summary of this class goes here
-  %   Detailed explanation goes here
   
   properties
     points, wts;
--- PDE1dImpl.m
+++ PDE1dImpl.m
@@ -1,7 +1,20 @@
-%% Copyright (C) 2016 bgreene
-%% 
-%% Author: bgreene <bgreene@BGREENE-HP>
-%% Created: 2016-01-07
+%  
+%   This library is free software; you can redistribute it and/or
+%   modify it under the terms of the GNU General Public License
+%   as published by the Free Software Foundation; either version 3
+%   of the License, or (at your option) any later version.
+%  
+%   This library is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%   General Public License for more details.
+%  
+%   You should have received a copy of the GNU General Public License
+%   along with this library; if not, visit
+%   http://www.gnu.org/licenses/gpl.html or write to the Free Software
+%   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+% 
+% Copyright (C) 2016-2019 William H. Greene
 
 classdef PDE1dImpl < handle
 
